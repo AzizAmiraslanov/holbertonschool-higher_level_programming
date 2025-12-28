641805d3a140e442cbe97278cceed82dcca8c065
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2025-12-28 19:30:41 +0400

    Task 8 update

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
             print("{}".format(c), end="")
     print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
