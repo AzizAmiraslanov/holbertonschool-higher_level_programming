6c2e08aa3367747ec671cbf00b3e22715d10582c
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-02 15:19:18 +0400

    4 section Task 0 updated

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-def print_list_integer(my_list):
-    for i in my_list:
-        print("{:d}".format(i))
-
-
-print_list_integer([1, 2, 3])
-
-
-
-
+def print_list_integer(my_list=[]):
+    for number in my_list:
+        print("{}".format(number))
