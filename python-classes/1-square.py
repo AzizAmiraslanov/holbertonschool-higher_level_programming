89fb41c09cdbc5e3e2efe7506d55fc38ca29a49a
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-02 15:57:13 +0400

    5 section Task 1 update

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """Defines a Square class with a private size attribute."""
 
+
 class Square:
+    """Defines a square with a private size attribute."""
+
     def __init__(self, size):
         self.__size = size
