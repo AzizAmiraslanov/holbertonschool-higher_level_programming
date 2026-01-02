4b92a6d8676e56bfd88c5ea472f76ee4c7e9a829
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-02 13:37:41 +0400

    Task 9 UPDATED

--- python-import_modules/103-fast_alphabet.py
+++ python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-__import__(bytes((115, 121, 115)).decode()).stdout.buffer.write(bytes(range(65, 91)) + bytes((10,)))
+__import__(bytes((115, 121, 115)).decode()).stdout.buffer.write(
+    bytes(range(65, 91)) + bytes((10,))
+)
