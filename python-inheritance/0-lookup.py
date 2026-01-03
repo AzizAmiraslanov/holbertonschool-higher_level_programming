52620600abbdf5504fab06669ccf3e064e4df37d
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-03 16:40:32 +0400

    9 section Task 0 updated

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 Returns the list of available attributes and methods of an object
 """
 
+
 def lookup(obj):
     """Return the list of available attributes and methods of an object"""
     return dir(obj)
