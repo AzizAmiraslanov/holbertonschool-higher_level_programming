a2926ba42406e22cdf3f25dc0a986e76b52d8381
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-10 15:46:03 +0400

    Section 11 Task 11 added

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with serialization and deserialization."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes listed in attrs
+        are included. Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    result[attr] = self.__dict__[attr]
+            return result
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance
+        using the given dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
