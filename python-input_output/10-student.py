8ca7956a53a803d6f06b69fb34afbd598f710a00
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-10 15:44:58 +0400

    Section 11 Task 10 added

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with JSON filtering."""
+
+
+class Student:
+    """Defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns the dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes listed in attrs
+        are included in the returned dictionary.
+        """
+        if isinstance(attrs, list):
+            result = {}
+            for attr in attrs:
+                if attr in self.__dict__:
+                    result[attr] = self.__dict__[attr]
+            return result
+        return self.__dict__
