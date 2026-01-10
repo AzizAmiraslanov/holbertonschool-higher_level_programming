539c9c5e39842d0f05ed389d349482b70c20aab1
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-10 15:33:59 +0400

    Section 11 Task 3 added

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module that provides a function to convert an object to a JSON string."""
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be converted to JSON
+
+    Returns:
+        A JSON string representation of my_obj
+    """
+    return json.dumps(my_obj)
