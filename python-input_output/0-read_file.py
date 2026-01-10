484bcd93fef87805c6d216102fe1f84dd070afad
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2026-01-10 15:51:13 +0400

    Section 11 Task 0 updated

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Module that defines a function to read and print a text file."""
+"""Module that reads a text file and prints it to stdout."""
 
 
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """Reads a text file (UTF8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
