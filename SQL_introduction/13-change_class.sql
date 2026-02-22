3276ab24a50935f5f4f8610c1846160fdf2edcec
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-23 01:22:44 +0400

    SQL_introduction 13 task finish

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,3 +1,9 @@
--- Script that removes all records with score <= 5
+-- Safe updates-i deaktiv et
+SET SQL_SAFE_UPDATES = 0;
+
+-- Row-ları sil
 DELETE FROM second_table
 WHERE score <= 5;
+
+-- Optional: geri aktiv et
+SET SQL_SAFE_UPDATES = 1;
