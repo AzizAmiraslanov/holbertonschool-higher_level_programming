3b8917f6bc8e4ad27134230af2041ac357a5d952
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-25 12:56:48 +0400

    python-object_relational_mapping 2 task update

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # SQL query using format
+    # SQL query: case-sensitive match
     query = (
-        "SELECT * FROM states WHERE name = '{}' "
+        "SELECT * FROM states WHERE BINARY name = '{}' "
         "ORDER BY id ASC;".format(state_name)
     )
     cursor.execute(query)
