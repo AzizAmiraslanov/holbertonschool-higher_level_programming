0571adc1a97073ddc1a8a999ea6ca3d8cd1f33b4
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-25 12:45:13 +0400

    python-object_relational_mapping 2 task update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-Lists all states starting with N from the database hbtn_0e_0_usa.
+Lists all states starting with N from the database provided as argument.
 """
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Komut satırından parametrləri alırıq
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # MySQL serverinə qoşuluruq
+    # Connect to MySQL
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -23,17 +22,15 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
 
-    # SQL sorğusu: adı N ilə başlayan ştatlar
-    query = "SELECT * FROM states "
-         "WHERE BINARY name LIKE 'N%' "
-         "ORDER BY id ASC;"
+    # SQL query: case-sensitive N
+    query = ("SELECT * FROM states "
+             "WHERE BINARY name LIKE 'N%' "
+             "ORDER BY id ASC;")
     cursor.execute(query)
 
-    # Nəticələri yazdırırıq
     results = cursor.fetchall()
     for row in results:
         print(row)
 
-    # Bağlantını bağlayırıq
     cursor.close()
     db.close()
