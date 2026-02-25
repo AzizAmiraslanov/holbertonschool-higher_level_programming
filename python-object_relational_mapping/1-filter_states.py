39d4879c43b92dc3f0d9e80cffb0a440b16bc0f9
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-25 12:27:32 +0400

    python-object_relational_mapping 1 task update

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-
 """
 Lists all states starting with N from the database hbtn_0e_0_usa.
 """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
     # Komut satırından parametrləri alırıq
