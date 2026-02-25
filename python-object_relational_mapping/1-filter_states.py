62f6eb14f58e2d50ed17ba27ddf5952ce5baafc8
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-25 12:22:31 +0400

    python-object_relational_mapping 1 task finish

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Komut satırından parametrləri alırıq
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # MySQL serverinə qoşuluruq
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db="hbtn_0e_0_usa"
+    )
+
+    cursor = db.cursor()
+
+    # SQL sorğusu: adı N ilə başlayan ştatlar
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;"
+    cursor.execute(query)
+
+    # Nəticələri yazdırırıq
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    # Bağlantını bağlayırıq
+    cursor.close()
+    db.close()
