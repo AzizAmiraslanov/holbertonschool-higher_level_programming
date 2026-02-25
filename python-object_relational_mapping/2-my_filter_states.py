c01d675b3aacb8a5b43aa085ad9db4823ca28cb8
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-25 12:37:48 +0400

    python-object_relational_mapping 2 task finish

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Lists all states with a name matching the argument from the database.
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    # SQL query with format to include user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
