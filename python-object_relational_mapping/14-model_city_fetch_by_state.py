c4106d74a23a9af3ccd34b57ad2fc6a263f19c77
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-02-25 14:46:42 +0400

    python-object_relational_mapping 14 update

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,27 +7,24 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    # Komut satırı argumentləri
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    # MySQL serverinə qoşulma
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database),
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database
+        ),
         pool_pre_ping=True
     )
 
-    # Session yaradılır
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Bütün şəhərləri id sırasına görə gətiririk
     cities = session.query(City).order_by(City.id).all()
 
     for city in cities:
-        # State adını göstərmək üçün əlaqəli State obyektini tapırıq
         state = session.query(State).get(city.state_id)
-        print(f"{state.name}: ({city.id}) {city.name}")
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,6 +3,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
+
 class City(Base):
     """City class mapping to 'cities' table"""
     __tablename__ = 'cities'
