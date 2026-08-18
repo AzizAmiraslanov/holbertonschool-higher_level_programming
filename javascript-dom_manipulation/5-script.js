1fa3b50aec1458ae169a47ae172fe7fdd720b519
Aziz <MSU-Rector@Aziz-Amiraslanov.local>
2026-08-18 19:38:25 +0400

    holbertonschool-higher_level_programming/javascript-dom_manipulation

---
+++ javascript-dom_manipulation/5-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  const updateHeader = document.querySelector('#update_header');
+  const header = document.querySelector('header');
+
+  updateHeader.addEventListener('click', function () {
+    header.textContent = 'New Header!!!';
+  });
+});
---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+document.addEventListener('DOMContentLoaded', function () {
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      const listMovies = document.querySelector('#list_movies');
+
+      data.results.forEach(movie => {
+        const listItem = document.createElement('li');
+        listItem.textContent = movie.title;
+        listMovies.appendChild(listItem);
+      });
+    });
+});
